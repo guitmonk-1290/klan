a205b1d7b46747ddb0af968a19e6d8641d7658ef
guitmonk-1290 <adityarana1290@gmail.com>
2023-11-15 03:05:05 +0530

    added mongoose.ts

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -1,3 +1,19 @@
 import mongoose from 'mongoose'
 
 let isConnected = false;
+
+export const connectToDB = async () => {
+    mongoose.set('strictQuery', true);
+
+    if (!process.env.MONGODB_URL) return console.log("MONGODB_URL not found");
+    if (isConnected) return console.log("Already connected to MongoDB");
+
+    try {
+        await mongoose.connect(process.env.MONGODB_URL);
+
+        isConnected = true;
+        console.log("[+] MongoDB connected!!");
+    } catch (error) {
+        console.log("[ERROR] connectToDB failed: ", error);
+    }
+}
