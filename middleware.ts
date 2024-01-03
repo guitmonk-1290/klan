47e3d591d6bb84abc85843a8ad8782fb7ba7c86d
aditya <adityarana1290@gmail.com>
2024-01-03 17:50:03 +0530

    middleware ignore route

--- middleware.ts
+++ middleware.ts
@@ -5,7 +5,7 @@ import { authMiddleware } from "@clerk/nextjs";
 // See https://clerk.com/docs/references/nextjs/auth-middleware for more information about configuring your Middleware
 export default authMiddleware({
     publicRoutes: ['/', 'api/webhook/clerk'],
-    ignoredRoutes: ['/api/webhook/clerk']
+    //ignoredRoutes: ['/api/webhook/clerk']
 });
  
 export const config = {
