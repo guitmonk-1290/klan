87699be2421cfe831bf70d5905d3a3e882266ca2
aditya <adityarana1290@gmail.com>
2024-01-07 21:10:00 +0530

    minor ui fixes

--- components/shared/RightSidebar.tsx
+++ components/shared/RightSidebar.tsx
@@ -11,7 +11,7 @@ async function RightSidebar() {
 
     return (
         <section className="custom-scrollbar rightsidebar">
-            <div className="flex flex-col bg-gradient-to-r from-pink-700 via-yellow-700 to-purple-700 p-2 rounded-xl">
+            <div className="flex flex-col bg-gradient-to-r from-pink-700 via-yellow-700 to-purple-700 p-2">
                 <h3 className="text-light-1 text-heading4-medium">Top Klans</h3>
             </div>
             <div className="flex flex-col gap-4">
@@ -30,15 +30,19 @@ async function RightSidebar() {
                                             className="rounded-full"
                                         />
                                     </div>
-                                    <div className="pt-1">{community.name}</div>
+                                    <div className="pt-2 text-[14px]">{community.name}</div>
                                 </div>
                             </div>
                         </Link>
                     ))
                 }
             </div>
-            <div className="flex flex-col gap-2 mt-4">
-                <button className="text-light-1 text-base-semibold bg-blue rounded-full hover:bg-transparent hover:border-[1px] hover:border-white transition duration-100 p-2">Explore Klans</button>
+            <div className="flex flex-col gap-2 mt-4 group relative">
+                <button
+                className="text-light-1 text-base-semibold bg-blue rounded-full hover:bg-transparent hover:border-[1px] hover:border-white transition duration-100 p-2">
+                    Explore Klans
+                </button>
+                <span className="absolute top-10 scale-0 rounded bg-gray-800 p-[2px] text-xs text-white group-hover:scale-100">✨ coming soon...</span>
             </div>
         </section>
     )
